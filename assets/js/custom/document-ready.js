c1b5b19850bc48d73294a36ec12130e9c97eb071
vitaliikaplia <vitalii.kaplia@gmail.com>
2026-03-05 00:37:06 +0200

    Refactor cookie popup styles and functionality, update SVG icon, and enhance accessibility features

--- assets/js/custom/document-ready.js
+++ assets/js/custom/document-ready.js
@@ -5,16 +5,17 @@
     $(document).ready(function () {
 
         /** Cookie Pop-Up */
-        if(!$.cookie("user-cookies-accepted") && $('.cookiePopupBg').length && $('.cookiePopupWrapper').length){
+        if(!$.cookie("user-cookies-accepted") && $('.cookiePopupWrapper').length){
             setTimeout(function(){
                 $('.cookiePopupWrapper').addClass('show');
-                $('.cookiePopupBg').addClass('show');
             }, 3000);
-            $('.cookiePopupWrapper a.close').click(function(){
-                $.cookie("user-cookies-accepted", true);
+            $('.cookiePopup .accept').click(function(){
+                $.cookie("user-cookies-accepted", true, {
+                    expires: 365,
+                    path: "/",
+                    secure: false
+                });
                 $('.cookiePopupWrapper').removeClass('show');
-                $('.cookiePopupBg').removeClass('show');
-                return false;
             });
         }
 
